41950c69e9b835abebd95fa495447433d80c9917
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-09-30 02:29:42 +0545

    Integrate the smart contract and allow waving

--- wave-portal-client/src/App.tsx
+++ wave-portal-client/src/App.tsx
@@ -1,12 +1,72 @@
 import "app/styles/App.css";
 import WaveCard from "./app/components/WaveCard";
 import ProfileSection from "./app/components/ProfileSection";
+import { useEffect, useState } from "react";
+interface IWindow {
+  ethereum: any;
+}
 function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window as unknown as IWindow;
+
+      if (!ethereum) {
+        return console.log("Make sure you have MetaMask");
+      } else {
+        console.log("MetaMask is already installed.", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Account found", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window as unknown as IWindow;
+
+      if (!ethereum) return alert("Install MetaMask extension first!");
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className="App bg-background min-h-screen flex flex-col sm:grid sm:grid-cols-12 lg:gap-6 gap-4 p-5 xl:p-10">
       <ProfileSection />
       <main className="p-4  border-secondary border-2 col-span-8 xl:col-span-9 rounded-lg">
-        <h3 className="text-3xl text-text font-semibold mb-4">Waves (32)</h3>
+        <div className="flex justify-between">
+          <h3 className="text-3xl text-text font-semibold mb-4">Waves (32)</h3>
+          {!currentAccount && (
+            <button
+              className="bg-primary px-3 py-2 rounded-md h-fit hover:bg-primary/40 transition-all"
+              onClick={connectWallet}
+            >
+              Connect wallet 💰
+            </button>
+          )}
+        </div>
         <hr className="border-text/40" />
         <div className="flex flex-col gap-4 mt-4 ">
           <WaveCard />
--- wave-portal-client/src/app/components/ProfileSection.tsx
+++ wave-portal-client/src/app/components/ProfileSection.tsx
@@ -1,8 +1,49 @@
-import React from "react";
-
+import React, { useState } from "react";
+import abi from "../utils/WavePortal.json";
+import { ethers } from "ethers";
 type Props = {};
+interface IWindow {
+  ethereum: any;
+}
 
-const ProfileSection = (props: Props) => {
+const ProfileSection = ({}: Props) => {
+  const [isMining, setMining] = useState(false);
+  const contactAddress = "0xF467F50A91F3C7294dF02b8E32DCd09c15d1DC04";
+  const contractABI = abi.abi;
+
+  const wave = async () => {
+    try {
+      const { ethereum } = window as unknown as IWindow;
+
+      if (ethereum) {
+        setMining(true);
+
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(
+          contactAddress,
+          contractABI,
+          signer
+        );
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Total waves ", count.toNumber());
+
+        let waveTxn = await wavePortalContract.wave();
+        console.log("Mining ", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined ", waveTxn.hash);
+        setMining(false);
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Total waves ", count.toNumber());
+      }
+    } catch (error) {
+      setMining(false);
+      console.log(error);
+    }
+  };
   return (
     <section className="border-2 border-secondary drop-shadow-lg col-span-4 xl:col-span-3 rounded-lg flex flex-col items-center px-2 sm:px-4 py-8 h-fit sm:sticky sm:top-5 xl:top-10">
       <img
@@ -13,8 +54,12 @@ const ProfileSection = (props: Props) => {
         Keshav Kishor Ram
       </h2>
       <i className="text-text text-md mb-4">@abhishekram404</i>
-      <button className="bg-secondary border-2  px-2 lg:px-4 py-2 w-3/4 rounded-lg hover:bg-primary text-text text-sm sm:text-sm lg:text-xl font-mono  tracking-widest ">
-        Wave me 👋
+      <button
+        className="bg-secondary border-2  px-2 lg:px-4 py-2 w-3/4 rounded-lg hover:bg-primary text-text text-sm sm:text-sm lg:text-xl font-mono  tracking-widest "
+        onClick={() => wave()}
+        disabled={isMining}
+      >
+        {isMining ? "Waving 👋" : "Wave me 👋"}
       </button>
     </section>
   );
